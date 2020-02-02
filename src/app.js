2759deff1b29700ec1fa46e42e7c4d41aea37de0
versave <vszhivkov@gmail.com>
2020-02-02 14:43:44 +0200

    Update 404 paths

--- src/app.js
+++ src/app.js
@@ -11,10 +11,10 @@ app.use(userRouter);
 app.use(productRouter);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('../client/build'));
 
     app.get('*', (res, req) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
     });
 }
 
